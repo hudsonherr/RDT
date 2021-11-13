6079e769918b4c4b30bbfa461bae8ee3af0e870c
hudsonherr <69992366+hudsonherr@users.noreply.github.com>
2021-11-12 16:30:59 -0800

    Add files via upload

---
+++ TCPL4.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class TCPL4
+{
+   public TCPL4()
+   {
+   
+   }
+   
+   //send is effectively implemented as UDT right now. It just passes the message down to layer 3 with nothing extra.
+   
+   //message is the message that sender is going to send
+   public int[] send(int message)
+   {
+      //you can have an array with more than 1 int for things like times / seq nums / etc.
+      int [] dataArray = new int[1];
+
+      dataArray[0] = message;
+      return dataArray;
+   }
+
+	 //receive is effectively implemented as UDT right now. It just passes the messages it gets up to layer 5 with nothing extra.
+
+   ///receive returns an arraylist of messages that it will send at the conclusion of the send call. The TCPL5 object is there only so you may send the messages up to layer 5. The boolean isSender indicates if the "this" object is the original sender or not.
+   public ArrayList<int []> receive(ArrayList<int [] >dataArray, TCPL5 reciever, boolean isSender) //isSender is for the purpose of letting you know if this is the sender of reciever
+   {
+      ArrayList<int []> responses = new ArrayList<int []>();
+
+      for(int i=0;i<dataArray.size();i++)
+      {
+         int [] DA = dataArray.get(i);
+         reciever.giveMessage(DA[0]); //send the data up to layer 5. Since the udt messages are 1 length, the data is at 0
+      }
+	  
+      return null; //you can return an arraylist of messages you want to send back. You know, acks, nacks, and whatnot
+   }
+   
+}
